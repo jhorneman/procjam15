1e6f00fcf875a9f119ea497340e3daea39fc8d6b
Jurie Horneman <jurie@jurie.org>
2015-11-05 14:36:54 +0100

    Worked on parsing. Reading all scene descriptions from scenes. Lots of error checking. Multiple scenes per file are possible. Data module can be run on its own. Moved scene file to subdirectory.

--- data.py
+++ data.py
@@ -2,10 +2,16 @@
 
 import sys
 import os
+import re
+import logging
 import xml.etree.ElementTree as ET
 
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(sys.argv[0])) + os.sep
+
+logger = logging.getLogger(__name__)
+
+scene_tag_re = re.compile(r"^\s*<scene", re.IGNORECASE)
 
 scenes = {}
 
@@ -13,43 +19,123 @@ scenes = {}
 class Option(object):
     def __init__(self):
         self.action = None
-        self.text = ''
+        self.text = ""
+
+    @staticmethod
+    def from_el(_el, _index):
+        new_option = Option()
+
+        new_option.action = _el.get("action")
+        if new_option.action is None:
+            logger.error("Option {0} does not contain an action attribute. Skipping.".format(_index))
+            return None
+
+        new_option.text = _el.text.strip()
+        if new_option.text is None or len(new_option.text) == 0:
+            logger.error("Option {0} does not contain any text. Skipping.".format(_index))
+            return None
+
+        return new_option
 
 
 class Scene(object):
     def __init__(self):
         self.id = None
-        self.desc = ''
-        self.short_desc = ''
-        self.optionals = []
+        self.desc = ""
+        self.short_desc = ""
         self.options = []
 
-    @staticmethod
-    def from_text_file(_file):
+
+def read_scenes_from_text_file(_file):
+    # Read the entire text file.
+    data = _file.read()
+
+    # Does this file start with a <scene> element?
+    if scene_tag_re.match(data):
+        # Yes -> Then wrap it in a <data> element.
+        data = "<data>" + data + "</data>"
+    else:
+        # No -> Then wrap it in a <data> and a <scene> element.
+        data = "<data><scene>" + data + "</scene></data>"
+
+    # Iterate over all scene elements in the text file.
+    root = ET.fromstring(data)
+    for scene_index, scene_el in enumerate(root.findall("scene")):
         new_scene = Scene()
 
-        data = '<text>' + _file.read() + '</text>'
-        root = ET.fromstring(data)
+        # Find meta element, skip if not found.
+        meta_el = scene_el.find("meta")
+        if meta_el is None:
+            logger.error("Scene {0} does not contain a meta element. Skipping.".format(scene_index+1))
+            continue
 
-        new_scene.id = root.find('meta').get('id')
+        # Get scene ID, skip if not found.
+        new_scene.id = meta_el.get("id")
+        if new_scene.id is None:
+            logger.error("Scene {0} has a meta element without an id attribute. Skipping.".format(scene_index+1))
+            continue
 
-        new_scene.desc = root.text if root.text is not None else ''
-        for child in root:
+        # Build scene description from all texts at the root of the scene element.
+        # This includes the tails of child elements.
+        new_scene.desc = scene_el.text if scene_el.text is not None else ""
+        for child in scene_el:
             if child.tail:
                 new_scene.desc += child.tail
 
-        new_scene.short_desc = root.find('repeat').text
-        new_scene.optionals.append(root.findall('optional'))
+        # Get short description, if any.
+        short_desc_el = scene_el.find("repeat")
+        if short_desc_el is not None:
+            new_scene.short_desc = short_desc_el.text
 
-        for option in root.findall('option'):
-            new_option = Option()
-            new_option.action = option.get('action')
-            new_option.text = option.text
-            new_scene.options.append(new_option)
+        # new_scene.optionals.append(scene_el.findall("optional"))
 
+        # Iterate over all option elements.
+        for option_index, option_el in enumerate(scene_el.findall("option")):
+            new_option = Option.from_el(option_el, option_index + 1)
+            if new_option:
+                new_scene.options.append(new_option)
+
+        if len(new_scene.options) == 0:
+            # Skip if no option elements were found.
+            logger.error("Scene {0} does not contain any valid option elements. Skipping.".format(scene_index+1))
+            continue
+
+        logger.info("Read scene {0}.".format(scene_index+1))
         scenes[new_scene.id] = new_scene
 
 
+def load_scene_descriptions():
+    # Iterate over all files in the scenes directory.
+    for path, dirs, files in os.walk(os.path.join(SCRIPT_DIR, "data", "scenes")):
+        for filename in files:
+            # Skip hidden files and anything not ending in .txt.
+            if filename.startswith("."):
+                continue
+            if not filename.endswith(".txt"):
+                continue
+
+            full_path = os.path.join(path, filename)
+            # scene_name = os.path.splitext(filename)[0]
+
+            # Open the file and read the scenes from it.
+            with open(full_path, "r") as f:
+                logger.info("Reading file {0}...".format(full_path))
+                read_scenes_from_text_file(f)
+
+
 def load_data():
-    with open(SCRIPT_DIR + 'data/test_scene.txt') as f:
-        Scene.from_text_file(f)
+    load_scene_descriptions()
+
+
+def set_up_console_logging():
+    global logger
+    logger.setLevel(logging.INFO)
+    console = logging.StreamHandler()
+    formatter = logging.Formatter("%(levelname)s: %(message)s")
+    console.setFormatter(formatter)
+    logger.addHandler(console)
+
+
+if __name__ == "__main__":
+    set_up_console_logging()
+    load_data()
