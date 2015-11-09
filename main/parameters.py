f0e9d578d9ad1cca4d875f49e1d028fe50e8f5b5
Jurie Horneman <jurie@jurie.org>
2015-11-09 17:14:33 +0100

    Distinguished between left hand and right hand side condition arguments.
    To better catch errors.

--- main/parameters.py
+++ main/parameters.py
@@ -7,23 +7,30 @@ import random
 logger = logging.getLogger(__name__)
 
 
-def get_parameter_value(_state, _parameter_name):
+def get_lhs_parameter_value(_state, _parameter_name):
     if _parameter_name.startswith("$"):
         variable_name = _parameter_name[1:]
         if variable_name in _state:
             return _state[variable_name]
         else:
-            logger.error("Could not find a state variable named '{0}' - using 0 instead.".format(variable_name))
+            logger.error("Could not find a state variable named '{0}'. A parameter on the left hand of an operator MUST refer to a state variable.".format(variable_name))
+            return None
+    else:
+        logger.error("A parameter on the left hand of an operator MUST refer to a state variable. '{0}' is invalid.".format(_parameter_name))
+        return None
+
+
+def get_rhs_parameter_value(_state, _parameter_name):
+    if _parameter_name.startswith("$"):
+        variable_name = _parameter_name[1:]
+        if variable_name in _state:
+            return _state[variable_name]
+        else:
+            logger.error("Could not find a state variable named '{0}'.".format(variable_name))
             return 0
 
     if _parameter_name.lower() == "random":
         return random.randint(0, 100)
-
-    if _parameter_name.lower() == "true":
-        return True
-
-    if _parameter_name.lower() == "false":
-        return False
 
     try:
         value = int(_parameter_name)
