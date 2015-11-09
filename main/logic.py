07edc62a050ed37e0bd119be5f6492a607c2a54d
Jurie Horneman <jurie@jurie.org>
2015-11-09 15:01:46 +0100

    Extracted text substitution into its own module.

--- main/logic.py
+++ main/logic.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import string
 import logging
-from flask import request, session, escape
+from flask import request, session
 from main.data import get_scene_description, get_scene_description_with_tag, Option
+from text_utils import substitute_text
 
 
 logger = logging.getLogger(__name__)
@@ -15,55 +15,6 @@ default_game_state = {
     "has_mcguffin": False,
     "amount_of_data": 0
 }
-
-
-class CustomFormatter(string.Formatter):
-    def get_value(self, key, args, kwargs):
-        # Original code from /lib/python2.7/string.py.
-        # If the key is a number, it's a positional argument, which we don't actually support,
-        # so just let this fail gracefully.
-        if isinstance(key, (int, long)):
-            return args[key]
-        else:
-            # First determine if we want the value to be capitalized.
-            capitalize = False
-            if key.startswith("^"):
-                capitalize = True
-                key = key[1:]
-
-            # Then remove the optional $ sign.
-            if key.startswith("$"):
-                key = key[1:]
-
-            # Do we have a value with this name?
-            if key in kwargs:
-                # Yes -> Get it.
-                value = kwargs[key]
-
-                # Convert it to a string if need be.
-                # TODO: Use isintance here
-                if type(value) != type(""):
-                    value = str(value)
-
-                # Capitalize it, if needed.
-                if capitalize:
-                    value = value[0].capitalize() + value[1:]
-
-                return value
-
-            else:
-                # No -> Complain.
-                logger.error("Text substitution key '{0}' was not found.".format(key))
-                return escape("<NOT FOUND>")
-
-formatter = CustomFormatter()
-
-
-def substitute_text(_text, _substitution_data):
-    if _text is None:
-        logger.error("Text to be converted is None.")
-        return ''
-    return formatter.vformat(_text, [], _substitution_data)
 
 
 def generate_player_character():
