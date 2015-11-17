b3193ffd931081d60f57ebc0987a531f7a008dc0
Jurie Horneman <jurie@jurie.org>
2015-11-17 12:36:22 +0100

    Tweaked error page, made it possible to return custom error messages from logic.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+import types
 from flask import render_template, abort, redirect, url_for, session
 from main.logic import get_current_scene_data
 from game_state import restart, get_game_state_vars
@@ -16,9 +17,11 @@ def inject_common_values():
 def show_scene():
     scene_data = get_current_scene_data()
     if scene_data:
-        return render_template("scene.html", **scene_data)
-    else:
-        abort(500)
+        if isinstance(scene_data, types.DictionaryType):
+            return render_template("scene.html", **scene_data)
+        if isinstance(scene_data, types.StringTypes):
+            return render_template("error.html", error=scene_data), 500
+    abort(500)
 
 
 @app.route("/restart")
@@ -34,9 +37,9 @@ def about():
 
 @app.errorhandler(500)
 def server_error(error):
-    return render_template("error.html", error_code=error), 500
+    return render_template("error.html", error=error), 500
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("error.html", error_code=error), 404
+    return render_template("error.html", error=error), 404
